d6a356eca9385d153880c7dfd424725ea1bd98e5
Osamu Nagano <onagano.g@gmail.com>
2013-05-23 14:43:32 +0900

    added security feature to SecureCalculator

--- ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/secure/SecureCalculatorBean.java
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/secure/SecureCalculatorBean.java
@@ -1,10 +1,14 @@
 package org.jboss.as.quickstarts.ejb.remote.secure;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import org.jboss.ejb3.annotation.SecurityDomain;
 
 @Stateless
 @Remote(SecureCalculator.class)
+@RolesAllowed({"guest"})
+@SecurityDomain("other")
 public class SecureCalculatorBean implements SecureCalculator {
 
     @Override
