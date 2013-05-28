5491c7c5dc08778778240c17196476806eb14b9d
Osamu Nagano <onagano.g@gmail.com>
2013-05-28 17:03:38 +0900

    clean up code

--- ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/secure/SampleLoginModule.java
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/secure/SampleLoginModule.java
@@ -18,49 +18,30 @@ import org.jboss.security.auth.spi.UsernamePasswordLoginModule;
 public class SampleLoginModule extends UsernamePasswordLoginModule
 {
     
-    /**
-     * Override to obtain the userPathPrefix and rolesPathPrefix options.
-     */
-    public void initialize(Subject subject, CallbackHandler callbackHandler,
-                           Map sharedState, Map options)
+    public void initialize(Subject subject, CallbackHandler callbackHandler, Map sharedState, Map options)
     {
         System.out.println("initialize");
-        try {
-            Map newOptions = new HashMap(options);
-        super.initialize(subject, callbackHandler, sharedState, newOptions);
-        } catch (Throwable e ){
-            e.printStackTrace();
-        }
+        super.initialize(subject, callbackHandler, sharedState, options);
     }
     
-    /**
-     *  Get the roles the current user belongs to by querying the
-     * rolesPathPrefix + '/' + super.getUsername() JNDI location.
-     */
     protected Group[] getRoleSets() throws LoginException
     {
-        String[] roles = new String[] { "all_users" };
+        String[] roles = new String[] {"guest"};
         Group[] groups = {new SimpleGroup("Roles")};
-        System.out.println("Getting roles for user="+super.getUsername());
-        for(int r = 0; r < roles.length; r ++) {
+        System.out.println("Getting roles for user=" + super.getUsername());
+        for(int r = 0; r < roles.length; r++) {
             SimplePrincipal role = new SimplePrincipal(roles[r]);
-            System.out.println("Found role="+roles[r]);
+            System.out.println("Found role=" + roles[r]);
             groups[0].addMember(role);
         }
         return groups;
     }
                     
-    /** 
-     * Get the password of the current user by querying the
-     * userPathPrefix + '/' + super.getUsername() JNDI location.
-     */
-    protected String getUsersPassword() 
-        throws LoginException
+    protected String getUsersPassword() throws LoginException
     {
-        System.out.println("Getting password for user="+super.getUsername());
-        String passwd = "admin";
-        System.out.println("Found password="+passwd);
+        System.out.println("Getting password for user=" + super.getUsername());
+        String passwd = "quickstartPwd1!";
+        System.out.println("Found password=" + passwd);
         return passwd;
-        
     }
 }
